ecab02c56c4e8fa00ee21d67b3fb799f5f632cf9
nik.6348 <nikhil@singaji.in>
2026-01-11 14:08:09 +0530

    feat: add favicon

--- src/controllers/linkController.js
+++ src/controllers/linkController.js
@@ -186,7 +186,8 @@ export const redirectLink = async (req, res) => {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>${link.title} | Redirecting...</title>
+    <title>${link.title}</title>
+    <link rel="icon" href="${link.favicon}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;600&display=swap" rel="stylesheet">
     <style>
